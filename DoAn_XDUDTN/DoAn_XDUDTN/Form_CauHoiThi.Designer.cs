8ec1caf8a49e808512354c25a387ace00796d7c7
Tuantai2000 <72971837+Tuantai2000@users.noreply.github.com>
2020-12-02 18:23:50 +0700

    first commit

---
+++ DoAn_XDUDTN/DoAn_XDUDTN/Form_CauHoiThi.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace DoAn_XDUDTN
+{
+    partial class Form_CauHoiThi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.lb_NgayThi = new System.Windows.Forms.Label();
+            this.lb_mon = new System.Windows.Forms.Label();
+            this.lb_KyThi = new System.Windows.Forms.Label();
+            this.lb_ngsinh = new System.Windows.Forms.Label();
+            this.lb_mats = new System.Windows.Forms.Label();
+            this.lb_Tensv = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.lb_NgayThi);
+            this.groupBox1.Controls.Add(this.lb_mon);
+            this.groupBox1.Controls.Add(this.lb_KyThi);
+            this.groupBox1.Controls.Add(this.lb_ngsinh);
+            this.groupBox1.Controls.Add(this.lb_mats);
+            this.groupBox1.Controls.Add(this.lb_Tensv);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(4, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(871, 198);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông Tin ";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(653, 119);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(54, 17);
+            this.label12.TabIndex = 17;
+            this.label12.Text = "label12";
+            // 
+            // lb_NgayThi
+            // 
+            this.lb_NgayThi.AutoSize = true;
+            this.lb_NgayThi.Location = new System.Drawing.Point(389, 119);
+            this.lb_NgayThi.Name = "lb_NgayThi";
+            this.lb_NgayThi.Size = new System.Drawing.Size(54, 17);
+            this.lb_NgayThi.TabIndex = 14;
+            this.lb_NgayThi.Text = "label12";
+            // 
+            // lb_mon
+            // 
+            this.lb_mon.AutoSize = true;
+            this.lb_mon.Location = new System.Drawing.Point(389, 78);
+            this.lb_mon.Name = "lb_mon";
+            this.lb_mon.Size = new System.Drawing.Size(54, 17);
+            this.lb_mon.TabIndex = 13;
+            this.lb_mon.Text = "label11";
+            // 
+            // lb_KyThi
+            // 
+            this.lb_KyThi.AutoSize = true;
+            this.lb_KyThi.Location = new System.Drawing.Point(389, 36);
+            this.lb_KyThi.Name = "lb_KyThi";
+            this.lb_KyThi.Size = new System.Drawing.Size(63, 17);
+            this.lb_KyThi.TabIndex = 12;
+            this.lb_KyThi.Text = "lb_KyThi";
+            // 
+            // lb_ngsinh
+            // 
+            this.lb_ngsinh.AutoSize = true;
+            this.lb_ngsinh.Location = new System.Drawing.Point(137, 119);
+            this.lb_ngsinh.Name = "lb_ngsinh";
+            this.lb_ngsinh.Size = new System.Drawing.Size(54, 17);
+            this.lb_ngsinh.TabIndex = 11;
+            this.lb_ngsinh.Text = "label12";
+            // 
+            // lb_mats
+            // 
+            this.lb_mats.AutoSize = true;
+            this.lb_mats.Location = new System.Drawing.Point(137, 78);
+            this.lb_mats.Name = "lb_mats";
+            this.lb_mats.Size = new System.Drawing.Size(54, 17);
+            this.lb_mats.TabIndex = 10;
+            this.lb_mats.Text = "label11";
+            // 
+            // lb_Tensv
+            // 
+            this.lb_Tensv.AutoSize = true;
+            this.lb_Tensv.Location = new System.Drawing.Point(137, 36);
+            this.lb_Tensv.Name = "lb_Tensv";
+            this.lb_Tensv.Size = new System.Drawing.Size(54, 17);
+            this.lb_Tensv.TabIndex = 9;
+            this.lb_Tensv.Text = "label10";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(576, 119);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(71, 17);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Thời gian:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(576, 78);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 17);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Đóng đề:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(576, 36);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Mở đề:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(308, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Ngày Thi:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(308, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Môn:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(308, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Kỳ Thi:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ngày Sinh:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã Thí Sinh:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên Sinh Viên:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.groupBox2.Location = new System.Drawing.Point(882, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(286, 198);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(23, 97);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(132, 17);
+            this.label11.TabIndex = 3;
+            this.label11.Text = "Số câu hoàn thành:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(23, 36);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(129, 17);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "Thời Gian Làm Bài:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.ForeColor = System.Drawing.Color.Red;
+            this.groupBox3.Location = new System.Drawing.Point(4, 208);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(871, 318);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Câu hỏi:";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox4.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.groupBox4.Location = new System.Drawing.Point(882, 208);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(296, 326);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Trả Lời:";
+            // 
+            // Form_CauHoiThi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1180, 538);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form_CauHoiThi";
+            this.Text = "Form_CauHoiThi";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lb_ngsinh;
+        private System.Windows.Forms.Label lb_mats;
+        private System.Windows.Forms.Label lb_Tensv;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lb_NgayThi;
+        private System.Windows.Forms.Label lb_mon;
+        private System.Windows.Forms.Label lb_KyThi;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+    }
+}
---
+++ DoAn_XDUDTN/DoAn_XDUDTN/Form_CauHoiThi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAn_XDUDTN
+{
+    public partial class Form_CauHoiThi : Form
+    {
+        public Form_CauHoiThi()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
