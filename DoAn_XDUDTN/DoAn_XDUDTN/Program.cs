0c29ab91ead1e07ef71fa59951a94955dc19baf8
Taipd0312 <58289247+Taipd0312@users.noreply.github.com>
2020-12-12 00:31:33 +0700

    form de thi

--- DoAn_XDUDTN/DoAn_XDUDTN/Program.cs
+++ DoAn_XDUDTN/DoAn_XDUDTN/Program.cs
@@ -16,7 +16,7 @@ namespace DoAn_XDUDTN
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmCauHoi());
+            Application.Run(new frnDeThi());
         }
     }
 }
---
+++ DoAn_XDUDTN/DoAn_XDUDTN/frnDeThi.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace DoAn_XDUDTN
+{
+    partial class frnDeThi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbmBox_MonHoc = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_MaDe = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_TaoMoi = new System.Windows.Forms.Button();
+            this.txt_SoCauHoi = new System.Windows.Forms.TextBox();
+            this.btn_Luu = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.gview_DsDe = new System.Windows.Forms.DataGridView();
+            this.gBox_DeThi = new System.Windows.Forms.GroupBox();
+            this.btn_Doi = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gview_DsDe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 7;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 66F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 238F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 52F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 234F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.btn_Doi, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.cbmBox_MonHoc, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txt_MaDe, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txt_SoCauHoi, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 6, 3);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 5, 1);
+            this.tableLayoutPanel1.Controls.Add(this.gBox_DeThi, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(1, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(933, 528);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Môn Học";
+            // 
+            // cbmBox_MonHoc
+            // 
+            this.cbmBox_MonHoc.FormattingEnabled = true;
+            this.cbmBox_MonHoc.Location = new System.Drawing.Point(69, 3);
+            this.cbmBox_MonHoc.Name = "cbmBox_MonHoc";
+            this.cbmBox_MonHoc.Size = new System.Drawing.Size(232, 21);
+            this.cbmBox_MonHoc.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label2.Location = new System.Drawing.Point(307, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mã Đề";
+            // 
+            // txt_MaDe
+            // 
+            this.txt_MaDe.Location = new System.Drawing.Point(359, 3);
+            this.txt_MaDe.Name = "txt_MaDe";
+            this.txt_MaDe.ReadOnly = true;
+            this.txt_MaDe.Size = new System.Drawing.Size(228, 20);
+            this.txt_MaDe.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(593, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Số Câu Hỏi";
+            // 
+            // btn_TaoMoi
+            // 
+            this.btn_TaoMoi.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_TaoMoi.Location = new System.Drawing.Point(84, 3);
+            this.btn_TaoMoi.Name = "btn_TaoMoi";
+            this.btn_TaoMoi.Size = new System.Drawing.Size(75, 23);
+            this.btn_TaoMoi.TabIndex = 6;
+            this.btn_TaoMoi.Text = "Tạo Mới";
+            this.btn_TaoMoi.UseVisualStyleBackColor = true;
+            // 
+            // txt_SoCauHoi
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.txt_SoCauHoi, 2);
+            this.txt_SoCauHoi.Location = new System.Drawing.Point(698, 3);
+            this.txt_SoCauHoi.Name = "txt_SoCauHoi";
+            this.txt_SoCauHoi.Size = new System.Drawing.Size(232, 20);
+            this.txt_SoCauHoi.TabIndex = 5;
+            // 
+            // btn_Luu
+            // 
+            this.btn_Luu.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Luu.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btn_Luu.Location = new System.Drawing.Point(3, 3);
+            this.btn_Luu.Name = "btn_Luu";
+            this.btn_Luu.Size = new System.Drawing.Size(75, 23);
+            this.btn_Luu.TabIndex = 7;
+            this.btn_Luu.Text = "Lưu";
+            this.btn_Luu.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.btn_Luu);
+            this.panel1.Controls.Add(this.btn_TaoMoi);
+            this.panel1.Location = new System.Drawing.Point(768, 496);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(162, 29);
+            this.panel1.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.groupBox1, 2);
+            this.groupBox1.Controls.Add(this.gview_DsDe);
+            this.groupBox1.Location = new System.Drawing.Point(698, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.tableLayoutPanel1.SetRowSpan(this.groupBox1, 2);
+            this.groupBox1.Size = new System.Drawing.Size(232, 454);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Danh Sách Đề Thi";
+            // 
+            // gview_DsDe
+            // 
+            this.gview_DsDe.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gview_DsDe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gview_DsDe.Location = new System.Drawing.Point(6, 19);
+            this.gview_DsDe.Name = "gview_DsDe";
+            this.gview_DsDe.Size = new System.Drawing.Size(220, 429);
+            this.gview_DsDe.TabIndex = 0;
+            // 
+            // gBox_DeThi
+            // 
+            this.gBox_DeThi.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.gBox_DeThi, 5);
+            this.gBox_DeThi.Location = new System.Drawing.Point(3, 36);
+            this.gBox_DeThi.Name = "gBox_DeThi";
+            this.tableLayoutPanel1.SetRowSpan(this.gBox_DeThi, 2);
+            this.gBox_DeThi.Size = new System.Drawing.Size(689, 454);
+            this.gBox_DeThi.TabIndex = 10;
+            this.gBox_DeThi.TabStop = false;
+            this.gBox_DeThi.Text = "Đề Thi";
+            // 
+            // btn_Doi
+            // 
+            this.btn_Doi.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_Doi.Location = new System.Drawing.Point(3, 496);
+            this.btn_Doi.Name = "btn_Doi";
+            this.btn_Doi.Size = new System.Drawing.Size(60, 29);
+            this.btn_Doi.TabIndex = 0;
+            this.btn_Doi.Text = "Đổi";
+            this.btn_Doi.UseVisualStyleBackColor = true;
+            // 
+            // frnDeThi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(935, 530);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "frnDeThi";
+            this.Text = "frnDeThi";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gview_DsDe)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbmBox_MonHoc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_MaDe;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_TaoMoi;
+        private System.Windows.Forms.TextBox txt_SoCauHoi;
+        private System.Windows.Forms.Button btn_Luu;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView gview_DsDe;
+        private System.Windows.Forms.Button btn_Doi;
+        private System.Windows.Forms.GroupBox gBox_DeThi;
+    }
+}
